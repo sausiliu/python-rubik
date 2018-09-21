4828810a00c0d1129f7f12d75375f53c20640807
Richard <sausiliu@163.com>
2018-09-21 14:06:18 +0800

    Fixed No module named pip.req

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
-from pip.req import parse_requirements
+try:
+    from pip._internal.req import parse_requirements
+except ImportError:# for pip <= 9.0.3
+    from pip.req import parse_requirements
 from setuptools import setup, find_packages
 from os import path
 from codecs import open
